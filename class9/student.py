d3d87357d00e88b7f2c46f768e363e6062c0a01b
MOHSIN ALI SOOMRO <39213044+MohsinAliSoomro@users.noreply.github.com>
2025-02-01 15:54:50 +0500

    Update methods

--- class9/student.py
+++ class9/student.py
@@ -2,17 +2,22 @@
 class Student:
     def __init__(self,name,age):
         self.name = name
-        self.ge = age
+        self.age = age
         print("run constructor")
 
     def get_info(self):
         print(f"Name {self.name} and age {self.age}")
+    def update_age(self,age:int):
+        self.age = age
+    def update_name(self,name:str):
+        self.name = "Name"
 """ End of class """
 
 
 s1 = Student("Mohsin",27)
 s2 = Student("Ali",28)
 
-# print(s1.__age)
+s1.update_age(30)
+# print(s1.age)
 s1.get_info()
 s2.get_info()
